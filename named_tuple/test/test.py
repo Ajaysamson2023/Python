958f5f66778e7d9dd253b83ed5be885fd1dfc513
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-22 20:33:01 +0530

    This is test file for named_tuple problem

---
+++ named_tuple/test/test.py
@@ -0,0 +1,14 @@
+import unittest
+from named_tuple.core.utils import *
+
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        actual_input=col_tuple()
+        expected_output=76.33333333333333
+        self.assertEqual(actual_input, expected_output)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
