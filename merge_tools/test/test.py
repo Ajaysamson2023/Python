7adad282beb1238111db9d66e03a3584c1159cce
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-22 20:32:19 +0530

    This is test file for merge_tools problem

---
+++ merge_tools/test/test.py
@@ -0,0 +1,15 @@
+import unittest
+from merge_tools.core.utils import *
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        actual_output = merge_the_tools(string, k)
+        expected_output = "AB"
+        self.assertEqual(actual_output, expected_output)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
