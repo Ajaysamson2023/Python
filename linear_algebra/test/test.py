c559c6a301bb05789264465d63c056a42f9a74bd
Ajaysamson2023 <ajay.k@diggibyte.com>
2023-05-22 20:32:02 +0530

    This is test file for linear_algebra problem

---
+++ linear_algebra/test/test.py
@@ -0,0 +1,14 @@
+import unittest
+from linear_algebra.core.utils import *
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        actual_input=linear_algebra()
+        expected_output=50.0
+        self.assertEqual(actual_input, expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
